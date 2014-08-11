c66068e899e87a3cdb94fad4d7dcde87edfc8a22
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2014-08-12 00:25:20 +0200

    Go a bit further in the dispatch

---
+++ src/jira_todoist/todoist.clj
@@ -0,0 +1,5 @@
+(ns jira-todoist.todoist)
+
+(defn assigneeChanged [ticket] "assignee changed")
+(defn summaryChanged [ticket] "summary changed")
+(defn descriptionChanged [ticket] "description changed")
--- src/jira_todoist/updatedTicket.clj
+++ src/jira_todoist/updatedTicket.clj
@@ -1,12 +1,26 @@
-(ns jira-todoist.updatedTicket)
+(ns jira-todoist.updatedTicket
+  (:require [jira-todoist.todoist :as todoist]))
+
+(def interestingFields ["assignee" "description" "summary"])
+
+(defn dispatchField [item ticket]
+    (case (get item :field)
+      ("assignee") (todoist/assigneeChanged ticket)
+      ("description") (todoist/descriptionChanged ticket)
+      ("summary") (todoist/summaryChanged ticket)
+      :IGNORED
+      ))
+
 
 (defn ticketValid? [ticket]
-  (def changes (get ticket :changelog))
-  (def items (get changes :items))
-  (def valid (some (fn [item] (= (get item :field) "assignee")) items))
+  (def items (get-in ticket [:changelog :items]))
+  (println (get (first items) :field))
+  (def valid (some (fn [item] (contains? (set interestingFields) (get item :field))) items))
   valid)
 
 (defn dispatchOnUpdate [ticket]
   "Dispatched on updated"
-  (if (ticketValid? ticket) :VALID)
-)
+  (if (ticketValid? ticket)
+    (map (fn [item] (dispatchField item ticket)) (get-in ticket [:changelog :items]))
+     :USELESS
+     ))
